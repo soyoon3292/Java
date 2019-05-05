f4680de54cc22b87aaa507d9cef34362b6bc54c3
soyoon3292 <jsp0916@gmail.com>
2019-05-06 01:15:54 +0900

    Modified.

--- VariableScopeExample.java
+++ VariableScopeExample.java
@@ -6,5 +6,6 @@ public class VariableScopeExample {
             v2 = v1 - 10;
         }
         int v3 = v1 + v2 + 5;   // v2 변수를 사용할 수 없기 때문에 컴파일 에러가 생김
+        System.out.println(v3);
     }
 }
